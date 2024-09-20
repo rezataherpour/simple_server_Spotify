c74eff5bd42cbd3ff6acf0e439cae33be22e9061
RezaThp <taherpourreza1998@gmail.com>
2024-09-20 18:20:37 -0400

    toggle a pin

--- main/main.c
+++ main/main.c
@@ -1,58 +1,28 @@
-#include "lvglGui.h"
-#include "GlobalInit.h"
-#include "nvsFlash.h"
-#include "WiFiConfig.h"
-#include "Authorization.h"
-#include "SpotifyInterface.h"
+#include <stdio.h>
+#include "driver/gpio.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
-#include "esp_http_client.h"
 
-#define TAG "MAIN"
-#define TIMER_TIME pdMS_TO_TICKS(500) // in millis
+#define GPIO_OUTPUT_PIN 14  // Use GPIO 13 for toggling
+#define GPIO_OUTPUT_PIN_SEL (1ULL << GPIO_OUTPUT_PIN)
 
-// ****************************** GLobal Variables ****************************** //
-//static const char *TAG = "Main";
-OAuthInterfaceHandler_t OAuthInterfaceHandler;
-SpotifyInterfaceHandler_t SpotifyInterfaceHandler;
-SpotifyHttpInfo_t Spotify_ClientInfo;
+void app_main(void) {
+    // Configure GPIO 13 as an output
+    gpio_config_t io_conf;
+    io_conf.intr_type = GPIO_INTR_DISABLE;   // Disable interrupts
+    io_conf.mode = GPIO_MODE_OUTPUT;         // Set as output mode
+    io_conf.pin_bit_mask = GPIO_OUTPUT_PIN_SEL; // Pin selection
+    io_conf.pull_down_en = 0;                // Disable pull-down mode
+    io_conf.pull_up_en = 0;                  // Disable pull-up mode
+    gpio_config(&io_conf);                   // Apply the configuration
 
-void app_main(void)
-{
-    GUI_TaskInit();
-    GlobalInit();
-    nvsFlashInit();
-    SpiffsGlobalConfig();
-#ifdef WIFI_INIT_STA_MODE
-    WifiStationMode("Done_IOT_1", "87654321@");
-    //WifiStationMode("BELL789", "167271A164A9");
-#else
-    wifiConnectionModule();
-#endif
+    int level = 0;  // Initial state
 
-#ifdef SpotifyEnable
+    while (1) {
+        // Toggle the output pin
+        level = !level;                      // Toggle level (0 -> 1, 1 -> 0)
+        gpio_set_level(GPIO_OUTPUT_PIN, level); // Write the new level
 
-//TODO merge all in a static function
-    OAuthInterfaceHandler.IsServiceAuthorizedSemaphore = &IsSpotifyAuthorizedSemaphore;
-    OAuthInterfaceHandler.ConfigAddressInSpiffs = SpotifyConfigAddressInSpiffs;
-    Spotify_ClientInfo = Spotify_ClientConfigInit();
-    OAuthInterfaceHandler.ClientConfig.url = Spotify_ClientInfo.url;
-    OAuthInterfaceHandler.ClientConfig.host = Spotify_ClientInfo.host;
-    OAuthInterfaceHandler.ClientConfig.path = Spotify_ClientInfo.path;
-    OAuthInterfaceHandler.ClientConfig.requestURI = Spotify_ClientInfo.requestURI;
-    OAuthInterfaceHandler.ClientConfig.responseURI = Spotify_ClientInfo.responseURI;
-    OAuthInterfaceHandler.ClientConfig.hostnameMDNS = Spotify_ClientInfo.hostnameMDNS;
-    OAuthInterfaceHandler.ClientConfig.requestURL = Spotify_ClientInfo.requestURL; 
-    OAuthInterfaceHandler.ClientConfig.clientID = Spotify_ClientInfo.clientID;
-    OAuthInterfaceHandler.ClientConfig.base64Credintials = Spotify_ClientInfo.base64Credintials;
-    OAuthInterfaceHandler.ClientConfig.redirectURL = Spotify_ClientInfo.redirectURL;
-
-    if(!Oauth_TaskInit(&OAuthInterfaceHandler))
-        ESP_LOGE(TAG, "OAuth task initialization failed.");
-
-    if (xSemaphoreTake(IsSpotifyAuthorizedSemaphore, portMAX_DELAY) == pdTRUE)
-    {
-        ESP_LOGW(TAG, "Access Token: %s", Oauth_GetAccessToken());
+        vTaskDelay(10 / portTICK_PERIOD_MS); // Add a 500ms delay
     }
-#endif
 }
